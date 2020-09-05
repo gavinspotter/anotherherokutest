760e32fc372ebd74fd3fd853d87c8cbf82e31ebd
Osiris <potterosiris@gmail.com>
2020-09-05 18:24:17 -0400

    server

---
+++ build/precache-manifest.32379eeeb56d44302faa060b8d73c63e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e03325854ee075725a8d364d6e977351",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b55ed49dbfc8deab5db8",
+    "url": "/static/css/main.3cfb82ee.chunk.css"
+  },
+  {
+    "revision": "e994da266a57f24c26f6",
+    "url": "/static/js/2.d14d0bb7.chunk.js"
+  },
+  {
+    "revision": "b55ed49dbfc8deab5db8",
+    "url": "/static/js/main.05ae8641.chunk.js"
+  },
+  {
+    "revision": "97c20e3509b70f348612",
+    "url": "/static/js/runtime-main.bcd39b9f.js"
+  }
+]);
